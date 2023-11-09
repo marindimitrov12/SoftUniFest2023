f5ed92bbc1161de46d435fbcd47b9904b8185068
marindimitrov12 <63950527+marindimitrov12@users.noreply.github.com>
2023-11-09 21:29:59 +0200

    create react app and import bootstrap theme to it

---
+++ SoftUniFest2023/softunifest2023_react/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter,Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Layout from './Components/Layout';
+function App() {
+  return (
+    <BrowserRouter >
+    <Routes>
+      <Route path="/" element={<Layout/>}>
+      <Route index element={<Home/>}/>
+      </Route>
+    </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
